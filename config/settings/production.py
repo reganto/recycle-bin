5a4b504df03fba634881ddf0a70dba3843906eb4
Reganto <tell.reganto@gmail.com>
2022-10-14 14:59:05 +0330

    Added logging section to production config

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,3 +4,29 @@ from .base import *
 DEBUG = config("DEBUG", default=False, cast=bool)
 STATIC_ROOT = BASE_DIR / "static"
 ALLOWED_HOSTS = [config("ALLOWED_HOSTS").split(" ")]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
+        }
+    },
+    'handlers': {
+        'gunicorn': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'verbose',
+            'filename': '/opt/djangoprojects/reports/bin/gunicorn.errors',
+            'maxBytes': 1024 * 1024 * 100,  # 100 mb
+        }
+    },
+    'loggers': {
+        'gunicorn.errors': {
+            'level': 'DEBUG',
+            'handlers': ['gunicorn'],
+            'propagate': True,
+        },
+    }
+}
